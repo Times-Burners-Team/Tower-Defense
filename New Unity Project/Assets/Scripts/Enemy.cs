c0660f54b40384a295168c6b872a3335962796f0
B3rl1 <kirill.skv.2000@mail.ru>
2018-04-06 20:17:54 +0300

    FirstChang

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public List<GameObject> WayPoints = new List<GameObject>();
+
+	public GameObject WayPointParent;
+
+	int wayIndex = 0;
+	public int speed;
+
+	
+	void Start () 
+	{
+		GetWay();
+	}
+	
+	
+	void Update () 
+	{
+		Move();	
+	}
+
+	void GetWay()
+	{
+		for(int i = 0; i < WayPointParent.transform.childCount; i++)
+		{
+			WayPoints.Add(WayPointParent.transform.GetChild(i).gameObject);
+		}
+	}
+	public void Move()
+	{
+		Vector2 dir = WayPoints[wayIndex].transform.position - transform.position;
+		transform.Translate(dir.normalized * Time.deltaTime * speed);
+
+		if(Vector3.Distance(transform.position , WayPoints[wayIndex].transform.position) < 0.5f)
+		{
+			if(wayIndex < WayPoints.Count - 1)
+			{
+				wayIndex++;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ New Unity Project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public float TimeToSpawn = 2;
+
+	int SpawnCount = 0;
+
+	public GameObject EnemyPref , WayPointParent;
+	
+	void Update ()
+	{
+		if(TimeToSpawn <= 0)
+		{
+			StartCoroutine(SpawnEnemy(SpawnCount + 1));
+			TimeToSpawn = 2;
+		}
+
+		TimeToSpawn -= Time.deltaTime;
+		GameObject.Find("SpawnTime").GetComponent<Text>().text = Mathf.Round(TimeToSpawn).ToString();
+	}
+
+	IEnumerator SpawnEnemy(int EnemyCount)
+	{
+		SpawnCount++;
+		for(int i = 0; i < EnemyCount; i++)
+		{
+			GameObject EnemyTmp = Instantiate(EnemyPref);
+			EnemyTmp.transform.SetParent(gameObject.transform, false);
+			EnemyTmp.GetComponent<Enemy>().WayPointParent = WayPointParent;
+
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+}
